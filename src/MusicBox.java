90aed61b8e68865a1f2630faa0594b6e9764044a
Legend0423 <2045133902@qq.com>
2025-02-21 11:35:20 +0800

    demo_branch

--- src/MusicBox.java
+++ src/MusicBox.java
@@ -12,7 +12,7 @@ public class MusicBox {
     public String songMusicLabel;
     public int noAds;
 
-    //Constructor
+    // Constructor
     public MusicBox(String songID, char premiumSong, String title, String artists, String Album, String genre, String producer, String label) {
         this.songID = songID;
         this.isSongPremium = premiumSong;
@@ -23,9 +23,9 @@ public class MusicBox {
         this.songProducer = producer;
         this.songMusicLabel = label;
     }
-    
-    
-    //Methods
+
+
+    // Methods
     public String getSongID() {
         return songID;
     }
@@ -37,6 +37,7 @@ public class MusicBox {
     public char getPremiumSong() {
         return isSongPremium;
     }
+
     public String getSongTitle() {
         return songTitle;
     }
@@ -61,34 +62,44 @@ public class MusicBox {
         return noAds;
     }
 
-    
-        /* The following method is complete. You don't have to do anything here. Just try to understand it. */
-        public void playSong(String songID, char premium, int ads) {
-            switch(premium){
-                case 'Y':
+
+    /* The following method is complete. You don't have to do anything here. Just try to understand it. */
+    public void playSong(String songID, char premium, int ads) {
+        switch (premium) {
+            case 'Y':
                 System.out.println("This is a premium song. Please buy it to play without the ads");
                 playAd(ads, premium);
                 break;
-                case 'N':
+            case 'N':
                 System.out.println("Thank you for choosing this song. Hope you enjoy listening to it.");
                 playAd(ads, premium);
                 break;
+        }
+    }
+
+
+    /* The following method is complete. You don't have to do anything here. Just try to understand it. */
+    private void playAd(int ads, char premium) {
+        if (ads == 1) {
+            System.out.println("Playing Ad 1");
+        } else {
+            for (int i = 1; i <= ads; i++) {
+                System.out.println("Playing Ad " + i);
             }
         }
 
+    }
 
-        /* The following method is complete. You don't have to do anything here. Just try to understand it. */
-        private void playAd(int ads, char premium) {
-            if (ads == 1) {
-                System.out.println("Playing Ad 1");
-            }
-            else {
-                for(int i=1; i<=ads; i++){
-                    System.out.println("Playing Ad " + i);
-                }
-            }
-            
-        }
-    
+    // 新增的 displaySongDetails 方法
+    public void displaySongDetails() {
+        System.out.println("Song ID: " + songID);
+        System.out.println("Is Premium: " + (isSongPremium == 'Y' ? "Yes" : "No"));
+        System.out.println("Title: " + songTitle);
+        System.out.println("Artists: " + songArtists);
+        System.out.println("Album: " + songAlbum);
+        System.out.println("Genre: " + songGenre);
+        System.out.println("Producer: " + songProducer);
+        System.out.println("Music Label: " + songMusicLabel);
+        System.out.println("Number of Ads: " + noAds);
+    }
 }
-

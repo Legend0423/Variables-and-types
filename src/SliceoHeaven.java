2e6122995fa8faae16bb56badcdbe110bbb3ea22
Legend0423 <2045133902@qq.com>
2025-02-25 11:27:40 +0800

    Slice-o-Heaven

--- src/SliceoHeaven.java
+++ src/SliceoHeaven.java
@@ -1,22 +1,99 @@
 public class SliceoHeaven {
+
     public String storeName;
     public String storeAddress;
     public String storeEmail;
     public String storePhone;
     public String storeMenu;
-    public String pizzaIngredients;
-    public double pizzaPrice;
-    public String sides;
-    public String drinks;
-    public String orderID;
-    public double orderTotal;
+
+    private String pizzaIngredients;
+    private double pizzaPrice;
+    private String sides;
+    private String drinks;
+    private String orderID;
+    private double orderTotal;
+
+    public static final String DEF_ORDER_ID = "DEF-SOH-099";
+    public static final String DEF_PIZZA_INGREDIENTS = "Mozzarella Cheese";
+    public static final double DEF_ORDER_TOTAL = 15.00;
+
+    public SliceoHeaven() {
+        this.orderID = DEF_ORDER_ID;
+        this.pizzaIngredients = DEF_PIZZA_INGREDIENTS;
+        this.orderTotal = DEF_ORDER_TOTAL;
+        this.sides = "";
+        this.drinks = "";
+    }
+
+    public SliceoHeaven(String orderID, String pizzaIngredients, double orderTotal) {
+        this.orderID = orderID;
+        this.pizzaIngredients = pizzaIngredients;
+        this.orderTotal = orderTotal;
+    }
+
+    public String getPizzaIngredients() {
+        return pizzaIngredients;
+    }
+
+    public void setPizzaIngredients(String pizzaIngredients) {
+        this.pizzaIngredients = pizzaIngredients;
+    }
+
+    public double getPizzaPrice() {
+        return pizzaPrice;
+    }
+
+    public void setPizzaPrice(double pizzaPrice) {
+        this.pizzaPrice = pizzaPrice;
+    }
+
+    public String getSides() {
+        return sides;
+    }
+
+    public void setSides(String sides) {
+        this.sides = sides;
+    }
+
+    public String getDrinks() {
+        return drinks;
+    }
+
+    public void setDrinks(String drinks) {
+        this.drinks = drinks;
+    }
+
+    public String getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(String orderID) {
+        this.orderID = orderID;
+    }
+
+    public double getOrderTotal() {
+        return orderTotal;
+    }
+
+    public void setOrderTotal(double orderTotal) {
+        this.orderTotal = orderTotal;
+    }
+
+    private void printReceipt() {
+        System.out.println("********RECEIPT********");
+        System.out.println("Order ID: " + orderID);
+        System.out.println("Pizza Ingredients: " + pizzaIngredients);
+        System.out.println("Order Total: " + orderTotal);
+        System.out.println("Sides: " + sides);
+        System.out.println("Drinks: " + drinks);
+    }
 
     public void takeOrder(String id, String ingredients, double total, String side, String drink) {
-        orderID = id;
-        pizzaIngredients = ingredients;
-        orderTotal = total;
-        sides = side;
-        drinks = drink;
+        setOrderID(id);
+        setPizzaIngredients(ingredients);
+        setOrderTotal(total);
+        setSides(side);
+        setDrinks(drink);
 
         System.out.println("Order accepted!");
         System.out.println("Order is being prepared");
@@ -31,15 +108,6 @@ public class SliceoHeaven {
     }
 
     public void makePizza() {
-        System.out.println("Making pizza with ingredients: " + pizzaIngredients);
-    }
-
-    public void printReceipt() {
-        System.out.println("********RECEIPT********");
-        System.out.println("Order ID: " + orderID);
-        System.out.println("Pizza Ingredients: " + pizzaIngredients);
-        System.out.println("Order Total: " + orderTotal);
-        System.out.println("Sides: " + sides);
-        System.out.println("Drinks: " + drinks);
+        System.out.println("Making pizza with ingredients: " + getPizzaIngredients());
     }
 }
